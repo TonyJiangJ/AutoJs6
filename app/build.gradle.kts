fd67109cdbe3976c63ab301250aed3eafe3c2667
SuperMonster003 <wu2du2mo2gu0@gmail.com>
2023-10-03 21:06:16 +0800

    PR 151 处理 - 恢复 OpenCV 线上部署; 统一代码风格

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -411,22 +411,13 @@ android {
         javaCompileOptions {
             annotationProcessorOptions {
                 mapOf(
-                    "resourcePackageName" to (this@defaultConfig.applicationId
-                        ?: this@Build_gradle.applicationId),
+                    "resourcePackageName" to (this@defaultConfig.applicationId ?: this@Build_gradle.applicationId),
                     "androidManifestFile" to ("$projectDir/src/main/AndroidManifest.xml")
                 ).let { arguments(it) }
             }
         }
-        buildConfigField(
-            "String",
-            "VERSION_DATE",
-            "\"${Utils.getDateString("MMM d, yyyy", "GMT+08:00")}\""
-        )
-        buildConfigField(
-            "String",
-            "VSCODE_EXT_REQUIRED_VERSION",
-            "\"${versions.vscodeExtRequiredVersion}\""
-        )
+        buildConfigField("String", "VERSION_DATE", "\"${Utils.getDateString("MMM d, yyyy", "GMT+08:00")}\"")
+        buildConfigField("String", "VSCODE_EXT_REQUIRED_VERSION", "\"${versions.vscodeExtRequiredVersion}\"")
     }
 
     applicationVariants.all {
@@ -441,9 +432,7 @@ android {
             }
         outputs
             .map { it as BaseVariantOutputImpl }
-            .forEach {
-                it.outputFileName = Utils.getOutputFileName(this@all as ApplicationVariantImpl, it)
-            }
+            .forEach { it.outputFileName = Utils.getOutputFileName(this@all as ApplicationVariantImpl, it) }
     }
 
     splits {
@@ -620,13 +609,7 @@ class Versions(filePath: String) {
         "Version name: $appVersionName",
         "Version code: $appVersionCode${" [auto-incremented]".takeIf { isBuildNumberAutoIncremented } ?: ""}",
         "SDK versions: min [$sdkVersionMin] / target [$sdkVersionTarget] / compile [$sdkVersionCompile]",
-        "Java version: $javaVersion${
-            " [fallback]".takeUnless {
-                javaVersion.isCompatibleWith(
-                    JavaVersion.toVersion(javaVersionRaw)
-                )
-            } ?: ""
-        }",
+        "Java version: $javaVersion${" [fallback]".takeUnless { javaVersion.isCompatibleWith(JavaVersion.toVersion(javaVersionRaw)) } ?: ""}",
     ).forEach { println(it) }
 
     fun handleIfNeeded(project: Project) = assembleTargets.forEach {
@@ -636,13 +619,7 @@ class Versions(filePath: String) {
             project.gradle.taskGraph.whenReady(object : Action<TaskExecutionGraph> {
                 override fun execute(taskGraph: TaskExecutionGraph) {
                     for (buildType in targetBuildType) {
-                        if (taskGraph.hasTask(
-                                Utils.getAssembleFullTaskName(
-                                    targetName,
-                                    buildType
-                                )
-                            )
-                        ) {
+                        if (taskGraph.hasTask(Utils.getAssembleFullTaskName(targetName, buildType))) {
                             return appendToTask(project, buildType)
                         }
                     }
@@ -662,8 +639,7 @@ object Utils {
 
     fun getDateString(format: String, zone: String): String {
         // e.g. May 23, 2011
-        return SimpleDateFormat(format).apply { timeZone = TimeZone.getTimeZone(zone) }
-            .format(Date())
+        return SimpleDateFormat(format).apply { timeZone = TimeZone.getTimeZone(zone) }.format(Date())
     }
 
     fun getOutputFileName(variant: ApplicationVariantImpl, output: BaseVariantOutputImpl): String {
@@ -677,8 +653,7 @@ object Utils {
 
     fun getAssembleTaskName(buildType: String) = "assemble${capitalize(buildType)}"
 
-    fun getAssembleFullTaskName(name: String, buildType: String) =
-        ":$name:${getAssembleTaskName(buildType)}"
+    fun getAssembleFullTaskName(name: String, buildType: String) = ":$name:${getAssembleTaskName(buildType)}"
 
     fun digestCRC32(file: File): String {
         val fis = FileInputStream(file)
